fde35925583bbf89601530ec504da6e4144fb230
Galina Antipin <galina.antipin@gmail.com>
2024-09-20 11:04:56 +0200

    connected my angularProject with firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,11 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"danotes-113b0","appId":"1:1006454733050:web:0f5a9efb61bd1e01933143","storageBucket":"danotes-113b0.appspot.com","apiKey":"AIzaSyDozmDn65VOy9IEzRAYSoVq72NpmKJOiDA","authDomain":"danotes-113b0.firebaseapp.com","messagingSenderId":"1006454733050"})), 
+    provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
 };
