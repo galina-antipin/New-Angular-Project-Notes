87ce9028cc7a250321058ffdb736a0ea57d064cc
Galina Antipin <galina.antipin@gmail.com>
2024-09-23 11:51:02 +0200

    ref notes and trash on firebase

--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -1,9 +1,26 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject, inject } from '@angular/core';
+import { Note } from '../interfaces/note.interface';
+import { Firestore, collection } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NoteListService {
+  trashNotes: Note[] = [];
+  normalNotes: Note[] = [];
+
+  firestore: Firestore = inject(Firestore);
 
   constructor() { }
+
+getNotesRef(){
+  return collection(this.firestore, 'notes');
+}
+
+getTrashRef(){
+  return collection(this.firestore, 'trash');
+}
+
+  
 }
